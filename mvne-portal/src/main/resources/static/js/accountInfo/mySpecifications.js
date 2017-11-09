7741eedfb6db2e73da2db1a929a9bf462dec9f1a
hansong <609670166@qq.com>
2017-11-09 22:05:04 +0800

    hansong create dev branch
    创建开发分支

---
+++ mvne-portal/src/main/resources/static/js/accountInfo/mySpecifications.js
@@ -0,0 +1,14 @@
+$(function () {
+  //日期选择器
+  laydate.render({
+    elem: '#speciStartDate', //指定元素
+    showBottom: false,
+    theme:"#4ca5fc"
+  });
+  //日期选择器
+  laydate.render({
+    elem: '#speciStopDate', //指定元素
+    showBottom: false,
+    theme:"#4ca5fc"
+  });
+});
