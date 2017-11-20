c025dc5e3594d1cee46c487fa128e2ba471f6430
zhuhuiqi <zhuhuiqi_2011@163.com>
2017-11-20 17:34:48 +0800

    首页部分模块的修改，优化其他小功能

--- mvne-portal/src/main/resources/static-zhq/js/homePage/homePage.js
+++ mvne-portal/src/main/resources/static-zhq/js/homePage/homePage.js
@@ -1,7 +1,7 @@
 $(function () {
   // 首页banner
   function carousel() {
-    var $window = $(window), window_width = $window.width() - 340;
+    var $window = $(window), window_width = $window.width() - 840;
     $('#js_banner, #js_banner_img li').width(window_width);
     new $.Tab({
       target: $('#js_banner_img li'),
@@ -21,36 +21,12 @@ $(function () {
     event.stopPropagation();
   });
   // 精选业务
-  /*function carouselBusiness() {
-    var len = $("#scrollPicsBusiness .num > li").length;
-    var index = 0;  //图片序号
-    var adTimer;
-    $("#scrollPicsBusiness .num li").mouseover(function() {
-      index = $("#scrollPicsBusiness .num li").index(this);  //获取鼠标悬浮 li 的index
-      showImg(index);
-    }).eq(0).mouseover();
-    //滑入停止动画，滑出开始动画.
-    $('#scrollPicsBusiness').hover(function() {
-      clearInterval(adTimer);
-    }, function() {
-      adTimer = setInterval(function() {
-        showImgBusiness(index)
-        index++;
-        if (index == len) {       //最后一张图片之后，转到第一张
-          index = 0;
-        }
-      }, 3000);
-    }).trigger("mouseleave");
-  }
-  carouselBusiness();
-  function showImgBusiness(index) {
-    var adWidth = $("#scrollPicsBusiness>ul>li:first").width();
-    $("#scrollPicsBusiness .slider").stop(true, false).animate({
-      "marginLeft": -adWidth * index + "px"    //改变 marginTop 属性的值达到轮播的效果
-    }, 1000);
-    $("#scrollPicsBusiness .num li").removeClass("on")
-      .eq(index).addClass("on");
-  }*/
+  $('#carousel ul').carouFredSel({
+    prev: '#prev',
+    next: '#next',
+    pagination: "#pager",
+    scroll: 1000
+  });
 
   // 号码推荐
   $("ul.numClass li").on("click",function () {
