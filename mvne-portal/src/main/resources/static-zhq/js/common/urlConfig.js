69e221b0c710c631c2a809598084d5b4a540436d
hansongj <hansong@chinamobilesz.com>
2018-07-10 10:08:12 +0800

    delete project

--- mvne-portal/src/main/resources/static-zhq/js/common/urlConfig.js
+++
@@ -1,3 +0,0 @@
-var url="http://192.168.115.71/"//访问路径头
-var phonenumber="homepage/phonenumber ",
-    RealNameAuthentication="homepage/RealNameAuthentication";
--- mvne-portal/src/main/resources/static-zhq/js/recharge/recharge.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-	$(".money ul li").click(function(){
-		$(this).find("a").addClass("on").parent("li").siblings().find("a").removeClass("on");
-	})
-})
